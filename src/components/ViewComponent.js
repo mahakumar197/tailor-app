a02861caa481136e618e486871a4a1159a9fb688
mahalakshmi <mahalakshmi.j@swordgroup.in>
2024-08-02 00:09:32 +0530

    view fix

--- src/components/ViewComponent.js
+++ src/components/ViewComponent.js
@@ -4,8 +4,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import debounce from "lodash/debounce"; // Import debounce from lodash
 import "./style.css";
-import arrow from "./arrow.png";
-import logo from "./logo.png";
+import arrow from "./arrowBlack.png";
+import Heart from "./Heart.png";
+import add from "./addbtn.png"
 
 function ViewComponent() {
   const [data, setData] = useState([]);
@@ -66,52 +67,62 @@ function ViewComponent() {
   };
 
   return (
-    
-      <div className="container">
-        <div className="d-flex justify-content-between pt-5">
-          <img src={logo} className="img-logo" alt="Logo" />
-          <Button variant="dark" className="btnn-arrow" onClick={handleLogout}>
-            <img src={arrow} className="ms-2" alt="arrow" />
+    <div className="container pt-5">
+      <div className="d-flex header-content justify-content-between">
+        <h1 className="header1-prop">Good Morning</h1>
+        <div className="d-flex mt-5">
+          <img src={Heart} className="me-3 img-heart" alt="fav" />
+          <p className="show-fav"> Show Favourites</p>
+        </div>
+      </div>
+      <div className="pt-4">
+        <h2 className="my-4 header3-prop">Search Customer</h2>
+        <div className="d-flex justify-content-between mb-4">
+          <input
+            type="text"
+            placeholder="Enter Customer's Phone Number Or Name"
+            className="form-control search-box"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <Button
+            variant="dark"
+            onClick={() => navigate("/create")}
+            className="btn-block btnn-create"
+          >
+            Add New
+            <img src={add} className="ms-2" alt="arrow" />
           </Button>
         </div>
-        <div className="pt-4">
-          <h1 className="my-4">Client Directory</h1>
-          <div className="mb-4">
-            <input
-              type="text"
-              placeholder="Search by name or mobile number"
-              className="form-control search-box"
-              value={searchTerm}
-              onChange={handleSearchChange}
-            />
-          </div>
-          <div className="row data-container">
-            {filteredData.length > 0 ? (
-              filteredData.map((item) => (
-                <div key={item.id} className="col-md-12 row mb-4">
-                  <div className="col-2">
-                    <img
-                      src={item.img}
-                      alt={item.Name}
-                      className="card-img-top"
-                    />
-                  </div>
-                  <div className="col-7 d-flex align-items-center row mb-5 ms-5">
-                    <h4>{item.phoneNumber || "N/A"}</h4>
-                    <h1>{item.Name}</h1>
-                    <h5>{item.address || "N/A"}</h5>
-                  </div>
-                  <div className="col-2 d-flex align-items-center">
-                    <Button
-                      as={Link}
-                      variant="dark"
-                      to={`/data/${item.id}`}
-                      className="btnn-view d-flex me-2 w-75"
-                    >
-                      View
-                      <img src={arrow} className="ms-3 w-25" alt="arrow" />
-                    </Button>
-                    {/* <Button
+
+        <div className="row data-container">
+          <h2 className="header4-prop">Customer List</h2>
+          {filteredData.length > 0 ? (
+            filteredData.map((item) => (
+              <div key={item.id} className="col-md-12 row mb-4">
+                <div className="col-2">
+                  <img
+                    src={item.img}
+                    alt={item.Name}
+                    className="card-img-top"
+                  />
+                </div>
+                <div className="col-7 d-flex align-items-center row mb-5 ms-5">
+                  <h4>{item.phoneNumber || "N/A"}</h4>
+                  <h1>{item.Name}</h1>
+                  <h5>{item.address || "N/A"}</h5>
+                </div>
+                <div className="col-2 d-flex align-items-center">
+                  <Button
+                    as={Link}
+                   
+                    to={`/data/${item.id}`}
+                    className="btnn-view d-flex me-2 w-75 fs-4"
+                  >
+                    View
+                    <img src={arrow} className="ms-3 w-25" alt="arrow" />
+                  </Button>
+                  {/* <Button
                       as={Link}
                       variant="warning"
                       to={`/edit/${item.id}`}
@@ -119,26 +130,15 @@ function ViewComponent() {
                     >
                       Edit
                     </Button> */}
-                  </div>
                 </div>
-              ))
-            ) : (
-              <h4>No results found</h4>
-            )}
-          </div>
-        </div>
-        <div className="fixed-bottom-btn w-50">
-          <Button
-            variant="dark"
-            onClick={() => navigate("/create")}
-            className="btn-block btnn-create"
-          >
-            Create New Customer
-            <img src={arrow} className="ms-2" alt="arrow" />
-          </Button>
-          
+              </div>
+            ))
+          ) : (
+            <h4>No results found</h4>
+          )}
         </div>
       </div>
+    </div>
     // </div>
   );
 }
